e3942067624fe89b0c4624b557cec466fd7195ff
roco <bob.cocksedge@gmail.com>
2021-05-14 17:50:59 +0100

    Complete to 4-17 of 130 all set up and working connection to noSQL index page Add data to db

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # noSQL
-"Firebase and Firestore database tutorials starting at Section 16 #127"
+<!-- "Firebase and Firestore database tutorials starting at Section 16 #127" -->
+<!-- "#127. NoSQL Databases" -->
+<!-- #128. Firebase & Firestore -->
+<!-- #129. Connecting to Firestore -->
+<!-- #130. Getting Collections -->
--- jscript.js
+++ jscript.js
@@ -1,1 +1,12 @@
 //Starting at Section 16 #127 Firebase and Firestore database tutorials
+//#127. NoSQL Databases
+//#128. Firebase & Firestore
+//#129. Connecting to Firestore
+//#130. Getting Collections
+
+db1.collection('small stories').get().then((snapshot) => {
+    //when data returned from db1
+    console.log(snapshot.docs[0].data());
+}).catch((err) => {
+    console.log(err)
+});
